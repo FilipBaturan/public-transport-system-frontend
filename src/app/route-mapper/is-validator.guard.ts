a60464b4222bd1fffd96167696ce84b111f5a46e
Filip <basarafilip@gmail.com>
2019-01-28 21:47:14 +0100

    Admin guard fixed

--- src/app/route-mapper/is-validator.guard.ts
+++ src/app/route-mapper/is-validator.guard.ts
@@ -14,7 +14,7 @@ export class IsValidatorGuard implements CanActivate {
       return true;
     }
 
-    this.router.navigateByUrl('signin');
+    this.router.navigateByUrl('welcome');
     return false;
   }
 }
